9214ee3b5a9d86feb4a9135e9f93bd856e212891
miya0001 <miya@wpist.me>
2015-03-07 14:37:57 +0900

    first commit

---
+++ wp-plugin-ci-demo.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Wp-plugin-ci-demo
+Version: 0.1-alpha
+Description: PLUGIN DESCRIPTION HERE
+Author: YOUR NAME HERE
+Author URI: YOUR SITE HERE
+Plugin URI: PLUGIN SITE HERE
+Text Domain: wp-plugin-ci-demo
+Domain Path: /languages
+*/
+
